5ce99c8d42a829eb3504d5584a0dc90cec668d9c
Chris Lovell <chrislovell@chriss-air.home>
2022-02-28 16:05:15 +0000

    created a test to ensure a top_up_card method can increase the balance of the oystercard. code to pass test created

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,7 +1,12 @@
 class Oystercard
-  attr_reader :balance
+  attr_accessor :balance
 
   def initialize(balance =0)
     @balance = balance
   end
+
+  def top_up_card(amount)
+    @balance += amount
+  end
+
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -4,4 +4,11 @@ describe Oystercard do
   it 'New card instance has a balance of 0' do
     expect(subject.balance).to eq 0
   end
+
+  it 'Oystercard to be topped up by £5' do
+    subject.top_up_card(5)
+    expect(subject.balance).to eq 5
+  end
+
+  
 end
