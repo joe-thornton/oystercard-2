851d2eac80f297ed0351ec8a2d241b9eaccafc5f
Joe Thornton <thornton.jp@gmail.com>
2022-02-28 16:50:13 +0000

    challenge 6: raise error if user tries to top up above max balance

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,12 +1,22 @@
 class Oystercard
   attr_accessor :balance
 
+  MAX_BALANCE = 90
+
   def initialize(balance =0)
     @balance = balance
+
   end
 
   def top_up_card(amount)
+    check_top_up_doesnt_exceed_max_balance(amount)
     @balance += amount
+  end
+
+  private
+
+  def check_top_up_doesnt_exceed_max_balance(amount)
+    raise "Top-up unsuccessful - balance cannot exceed £#{MAX_BALANCE}" if (@balance + amount) > MAX_BALANCE 
   end
 
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -9,6 +9,12 @@ describe Oystercard do
     subject.top_up_card(5)
     expect(subject.balance).to eq 5
   end
-
+  
+  it 'raises an error when top-up takes balance over £90' do
+    max_balance = Oystercard::MAX_BALANCE
+    subject.top_up_card(max_balance)
+    error_message = "Top-up unsuccessful - balance cannot exceed £#{max_balance}"
+    expect { subject.top_up_card(1) }.to raise_error error_message
+  end
   
 end
